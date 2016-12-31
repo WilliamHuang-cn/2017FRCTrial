13cf8a9812a55dc789852bd0e4eb90439e30e338
William Huang <huang.william.cn@hotmail.com>
2016-12-31 11:50:03 +0800

    Removes debugging outputs.

--- src/org/usfirst/frc/team1256/robot/Debug.java
+++ src/org/usfirst/frc/team1256/robot/Debug.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team1256.robot;
 import edu.wpi.first.wpilibj.Timer;
 
 public final class Debug {
-	public final static boolean ON = true;
+	public final static boolean ON = false;
 	public static long iterationCounter; 
 	
 	public static void writeLog(Object sender,String message) {
